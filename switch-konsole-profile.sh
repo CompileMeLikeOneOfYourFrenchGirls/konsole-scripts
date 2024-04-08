1dbb387d8826041839c4367d19e7a197d9f8e608
CompileMeLikeOneOfYourFrenchGirls <spk91jpn@gmail.com>
2024-04-08 12:18:19 +0200

    Better output, removed some inefficiencies

--- switch-konsole-profile.sh
+++ switch-konsole-profile.sh
@@ -1,46 +1,20 @@
 #!/bin/bash
 
 display_help () {
-  printf "Usage: ./switch-konsole-profile.sh [options] [arguments]\n"
-  printf "Where arguments can be one or more of the following:\n"
+  printf "This script switches the active profile for a KDE Konsole terminal between two hardcoded profiles.\n"
+  printf "Supply no arguments to automatically get the PID of the active terminal window.\n"
+  printf "Or supply a valid terminal window PID to change the profile of that specific window.\n"
   printf "\n"
-  printf "Process ID of konsole (terminal emulator) window.\n"
-  printf "Profile name to switch to.\n"
-  printf "Tab session number in case of multiple tabs.\n"
+  printf "This script is unable to select the active tab if the terminal window has multiple tabs,\n"
+  printf "it will always change the profile of the first tab, unless a tab number will be specified as the last argument to the script.\n"
+  printf "In that case, it will try to change that tab profile. It will fail if that number does not correspond to a valid tab session.\n"
   printf "\n"
-  printf "Options are: \n"
-  printf "To show available profiles: -l\n"
-  printf "To show this help: -h\n"
-  printf "To show full help: --help\n"
-  exit 0
-}
-display_help_full () {
-  printf "Usage: ./switch-konsole-profile.sh [options] [arguments]\n"
-  printf "Where arguments can be one or more of the following:\n"
+  printf "Working:\n"
   printf "\n"
-  printf "Process ID of konsole (terminal emulator) window.\n"
-  printf "Profile name to switch to.\n"
-  printf "Tab session number in case of multiple tabs.\n"
-  printf "\n"
-  printf "Options are: \n"
-  printf "To show available profiles: -l\n"
-  printf "To show this help: -h\n"
-  printf "To show full help: --help\n"
-  printf "\n"
-  printf "This script switches the active profile for a KDE Konsole terminal to a specified profile,\n"
-  printf "or if missing, between two hardcoded profiles.\n"
-  printf "It is able to retrieve the PID of the active terminal window automatically, and MAY be able to guess the correct\n"
-  printf "tab session in case of multiple tabs open in the same window.\n"
-  printf "\n"
-  printf "Is it also possible to supply a valid terminal window PID to change the profile of that specific window.\n"
-  printf "If no tab number is passed to the script,  it will try to change the profile of the first tab.\n"
-  printf "\n"
-  printf "We get the window process using:\n"
-  printf "\n"
+  printf "We get the window process using: \n"
   printf "xprop -id \$(xdpyinfo | grep -Eo 'window 0x[^,]+' | cut -d\" \" -f2) | grep PID\n"
   printf "\n"
   printf "Then we use the process number as argument for:\n"
-  printf "\n"
   printf "qdbus org.kde.konsole-PID /Sessions/1 profile\n"
   printf "\n"
   printf "to retrieve the current profile, and to:\n"
@@ -58,12 +32,12 @@ get_valid_tab () {
     tabsession=1
   fi
 
-  result="$(qdbus org.kde.konsole-$pid /Sessions/"$tabsession" profile)"
+  result="$(qdbus org.kde.konsole-"$pid" /Sessions/"$tabsession" profile)"
 
   while [[ "$result" == *"Error"* ]]; do
 
     ((++tabsession))
-    result="$(qdbus org.kde.konsole-$pid /Sessions/"$tabsession" profile)"
+    result="$(qdbus org.kde.konsole-"$pid" /Sessions/"$tabsession" profile)"
 
     if [[ "$tabsession" -gt 20 ]]; then
       printf "Error while searching for a valid tab session\n"
@@ -85,19 +59,19 @@ switch_konsole_profile () {
     #If the qdbus command returns an Error, most likely the tab number is incorrect
     #We increase the number until we find a valid tab session, or break the loop if we reach 20
 
-    
+
     if [[ $current_profile == "starship" ]] ; then
       newprofile="$(printf 'starship light')" ; else
       newprofile="$(printf 'starship')"
     fi
-    
-    printf "Switching to profile: $newprofile\n"
+
+    printf "Switching to profile: %s\n" "$newprofile"
     qdbus org.kde.konsole-"$pid" /Sessions/"$tabsession" setProfile "$newprofile"
     #printf "result: $?\n"
     exit 0
   fi
 
-  printf "Switching to profile: $profile\n"
+  printf "Switching to profile: %s\n" "$profile"
   qdbus org.kde.konsole-"$pid" /Sessions/"$tabsession" setProfile "$profile"
   #printf "result: $?\n"
   exit 0
@@ -106,9 +80,11 @@ switch_konsole_profile () {
 get_terminal_pid () {
 
   # This gets the PID of the current (active) window
-  local xdpyinfo_string="$(xdpyinfo | grep -Eo 'window 0x[^,]+' | cut -d" " -f2)"
-  local pidstring="$(xprop -id $xdpyinfo_string | grep PID)"
-  pid="$( echo ${pidstring//[^[:digit:]]/})"
+  local xdpyinfo_string
+  local pidstring
+  xdpyinfo_string="$(xdpyinfo | grep -Eo 'window 0x[^,]+' | cut -d" " -f2)"
+  pidstring="$(xprop -id "$xdpyinfo_string" | grep PID)"
+  pid="${pidstring//[^[:digit:]]/}"
   #printf "pid value is:\n"
   #printf "$pid\n"
 
@@ -123,15 +99,22 @@ list_profiles () {
   source_dir=$HOME/.local/share/konsole
   files=("$source_dir"/*.profile)
 
+  #files is an array containing the full path of every file ending in .profile
+  #we only need the filename without the extension
   for file in "${files[@]}"; do
-    y="${file##*/}"
-    file="${y%.profile}"
+    # Note that we need a temporary variable inside this block to correctly perform substitution before adding to array
+    # file##*/ removes the path from the file string
+    #filename="${file##*/}"
+    #alternatively, use basename
+    filename=$(basename "$file")
+    # filename%.profile removes the last occurrence of .profile from the file name
+    file="${filename%.profile}"
     filelist=("${filelist[@]}" "$file")
   done
 }
 
 confirm_profile () {
-  
+
   list_profiles
 
   for f in "${filelist[@]}"
@@ -148,7 +131,7 @@ confirm_profile () {
 check_pid () {
 
   pslist=$(pgrep -lf /usr/bin/konsole)
-  
+
   echo "$pslist" | grep -q "$pid"
 
   if ! [[ "$?" = "0" ]]; then
@@ -156,7 +139,7 @@ check_pid () {
     printf "Error, wrong PID number, falling back to this terminal emulator PID\n"
     printf "\n"
     get_terminal_pid
-    printf "PID value is: $pid\n"
+    printf "PID value is: %s\n" "$pid"
   fi
 
 }
@@ -167,10 +150,12 @@ profile=""
 
 main () {
 
+  local rest
   local arg1="$1"; shift
   local arg2="$1"; shift
   local arg3="$1"; shift
-  local rest="$@"
+
+  rest=$(IFS=, ; echo "$*")
   declare -a filelist
   declare -a files
 
@@ -183,25 +168,10 @@ main () {
 
 
   if [[ -n "$arg3" ]]; then
-    
+
     # this returns 0 (false) if left side matches right side
-    # or 1 (true) otherwise 
-    if [[ "$arg3" == "-h" ]]; then
-
-      display_help
-
-    elif [[ "$arg3" == "--help" ]]; then
-
-      display_help_full
-
-    elif [[ "$arg3" == "-l" ]]; then
-
-      list_profiles
-      printf "\n"
-      printf "$filelist\n"
-      exit 0
-
-    elif [[ "$arg3" =~ ^[0-9]$ ]]; then
+    # or 1 (true) otherwise
+    if [[ "$arg3" =~ ^[0-9]$ ]]; then
 
       tabsession="$arg3"
 
@@ -210,7 +180,7 @@ main () {
       pid="$arg3"
       check_pid
 
-      
+
     else
 
       #not a number, so treat it as a profile name
@@ -251,22 +221,7 @@ main () {
 
   if [[ -n "$arg2" ]]; then
 
-    if [[ "$arg2" == "-h" ]]; then
-
-      display_help
-
-    elif [[ "$arg2" == "--help" ]]; then
-
-      display_help_full
-
-    elif [[ "$arg2" == "-l" ]]; then
-
-      list_profiles
-      printf "\n"
-      printf "$filelist\n"
-      exit 0
-
-    elif [[ "$arg2" =~ ^[0-9]$ ]]; then
+    if [[ "$arg2" =~ ^[0-9]$ ]]; then
 
       tabsession="$arg2"
 
@@ -321,15 +276,11 @@ main () {
 
       display_help
 
-    elif [[ "$arg1" == "--help" ]]; then
-
-      display_help_full
-
     elif [[ "$arg1" == "-l" ]]; then
 
       list_profiles
-      printf "\n"
-      printf "$filelist\n"
+      printf "Available profiles:\n"
+      printf "%s\n" "${filelist[@]}"
       exit 0
 
     elif [[ "$arg1" =~ ^[0-9]+$ ]]; then
@@ -356,10 +307,10 @@ main () {
   if [[ -z "$pid" ]]; then
 
     get_terminal_pid
-    printf "PID value is: $pid\n"
+    printf "PID value is: %s\n" "$pid"
   fi
-  
-  
+
+
   switch_konsole_profile
 }
 
